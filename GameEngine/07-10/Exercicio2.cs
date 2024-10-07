f469cc2ec7e7be1d9ee39f7d2a424f8b190f8c29
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-10-07 16:53:46 -0300

    Create Exercicio2.cs

---
+++ GameEngine/07-10/Exercicio2.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Exercicio2 
+{
+    public static void Main (string[] args) {
+        int nota1 = 4;
+        double nota2 = 7.6;
+        double nota3 = 9.7;
+        double media;
+        media = nota1 + nota2 + nota3;
+
+        Console.WriteLine("A media do aluno e: {0}",media);
+
+    }
+}
