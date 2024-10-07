0dea91c9a584383923ae74a2484b5bdf091a24b8
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-10-07 17:17:57 -0300

    Update Exercicio2.cs

--- GameEngine/07-10/Exercicio2.cs
+++ GameEngine/07-10/Exercicio2.cs
@@ -11,5 +11,29 @@ public class Exercicio2
 
         Console.WriteLine("A media do aluno e: {0}",media);
 
+                int num1 = 4;
+        int num2 = 2;
+        int num3 = 6;
+        int num4 = 8;
+        int num5 = 10;
+        int num6 = 1;
+        int num7 = 3;
+        int num8 = 5;
+        int num9 = 7;
+        int num10 = 9;
+
+        Console.WriteLine(num1%2);
+        Console.WriteLine(num2%2);
+        Console.WriteLine(num3%2);
+        Console.WriteLine(num4%2);
+        Console.WriteLine(num5%2);
+        Console.WriteLine(num6%2);
+        Console.WriteLine(num7%2);
+        Console.WriteLine(num8%2);
+        Console.WriteLine(num9%2);
+        Console.WriteLine(num10%2);
+
+        Console.WriteLine("Para descobrir se o numero e par, o resto da divisão dele tem que ser igual a 0. E para descobrir se e impar, o resto da divisao dele tem que ser igual a 1.");
+
     }
 }
