5c965a0454ce6eec0d962300c6564998cc3ac7ca
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-10-07 15:19:58 -0300

    Create Exercicio1.cs

---
+++ GameEngine/07-10/Exercicio1.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Exercicio1 {
+
+    static void Main (String[] args) {
+        int numero1 = 23;
+        int numero2 = 52;
+        int numero3 = 21;
+        int numero4 = 75;
+        Console.WriteLine(numero1 + numero2);
+        Console.WriteLine(numero2 + numero3);
+        Console.WriteLine(numero3 + numero4);
+        Console.WriteLine(numero1 - numero2);
+        Console.WriteLine(numero2 - numero2);
+        Console.WriteLine(numero3 - numero4);
+        Console.WriteLine(numero1 / numero2);
+        Console.WriteLine(numero2 / numero3);
+        Console.WriteLine(numero3 / numero4);
+        Console.WriteLine(numero1 * numero2);
+        Console.WriteLine(numero2 * numero3);
+        Console.WriteLine(numero3 * numero4);
+    }
+
+}
