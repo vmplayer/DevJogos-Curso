05b432fdcaa1935421da766d1308b7e569545b6a
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-10-07 14:28:49 -0300

    Create teste.cs

---
+++ GameEngine/07-10/teste.cs
@@ -0,0 +1,10 @@
+using System;
+
+//Tudo em C# vai dentro das chamadas "classes".
+class HelloWorld {
+    //Tudo que eu quero que seja mostrado no terminal, sendo chamado ou não dentro de tal.
+    static void Main() {
+         //Imprime no console a frase "Hello, World!"
+        Console.WriteLine("Hello, World!");
+    }
+};
