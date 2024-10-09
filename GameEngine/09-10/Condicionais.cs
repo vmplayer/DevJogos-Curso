f36c8a02672c6d4625e0cd71f365290070ac4841
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-10-09 16:20:51 -0300

    Update Condicionais.cs

--- GameEngine/09-10/Condicionais.cs
+++ GameEngine/09-10/Condicionais.cs
@@ -6,16 +6,21 @@ class Condicionais {
 
         //Condicionais são estruturas que permitem que um bloco de código só seja executado SE obedecer uma determinada condição.
 
-        bool temCarteira = true;
+        bool temCarteira = false;
+        bool maioridade = 21 >= 18;
 
         //SE eu tenho carteira
         if (temCarteira)
-        //Bloco de código que será executado se a condição for verdadeira
+        //Bloco de código que será executado se a condição for verdadeira.
         {Console.WriteLine("Voce pode dirigir!");
-        //Se não tenho carteira
+        //OU
+        }else if (!temCarteira && maioridade) {
+            //Bloco de código executado quando apenas uma das condições for verdadeira.
+            Console.WriteLine("Voce tem idade suficiente, mas nao tem carteira, portanto, voce nao pode dirigir.");
+        //SENÃO
         }else{
-          //Bloco de código executado se a condição for falsa.
-            Console.WriteLine("Voce nao pode dirigir!");
+            //Bloco de código executado quando ambas condições forem falsas.
+            Console.WriteLine("Voce nao pode dirigir.");
         }
     }
 }
