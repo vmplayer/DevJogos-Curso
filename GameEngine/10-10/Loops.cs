6b62462091a78ab4d597996bc0d651d30efea1e8
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-10-10 14:54:39 -0300

    Create Loops.cs

---
+++ GameEngine/10-10/Loops.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Loops {
+
+    static void Main (String[] args) {
+
+        int voltas = 0;
+
+        while (voltas <= 10) {
+
+        Console.WriteLine($"Você ja deu {voltas} voltas. Pedale!");
+        voltas += 1;
+
+        }
+
+    }
+}
