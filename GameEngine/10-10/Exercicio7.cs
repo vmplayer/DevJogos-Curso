042599587ce7c3e152699dcdb0efe08ac5692291
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-10-10 16:44:22 -0300

    Create Exercicio7.cs

---
+++ GameEngine/10-10/Exercicio7.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Exercicio7 {
+    static void Main (String[] args) {
+
+        int numero = 4;
+        for (int i = 1; i <= 10; i++) {
+            Console.WriteLine($"{numero} x {i} = {numero * i}");
+        }
+    }
+}
