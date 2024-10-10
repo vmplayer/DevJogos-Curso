4894ba50a8a714d284fafa3edbb3cb2ba063b790
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-10-10 14:01:57 -0300

    Create Exercicio5.cs

---
+++ GameEngine/09-10/Exercicio5.cs
@@ -0,0 +1,26 @@
+//corrigir o script
+using System;
+class Execicio5 {
+
+    static void Main (String[] args) {
+
+        switch nacionalidade{
+
+            case "gremio";
+             Console.WriteLine("dale gremio");
+            break;
+
+            case "inter";
+             Console.WriteLine("inter nome de bancokkkkk");
+            break;
+
+            case "palmeiras";
+             Console.WriteLine("dale porco");
+            break;
+
+            default;
+             Console.WriteLine("ateu maldito.");
+            }
+        
+    }
+}
