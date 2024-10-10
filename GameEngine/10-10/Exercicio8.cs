1e9f810f73d4b2190bc6ffaa349082c690064c62
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-10-10 16:45:05 -0300

    Create Exercicio8.cs

---
+++ GameEngine/10-10/Exercicio8.cs
@@ -0,0 +1,19 @@
+//Faça um array contendo seus 5 jogos favoritos.
+//Imprima todo o Array
+//Depois, imprima um por um.
+
+using System;
+
+class Exercicio8 {
+
+    static void Main (String[] args) {
+
+        string[] jogos = {"Hollow Knight: Voidheart Edition", "CoD: Black Ops II", "Grand Theft Auto: San Andreas", "Super Coleção 7.784 Jogos", "Celeste"};
+
+        Console.WriteLine("Ou vai ser esse aqui: {0}",jogos[0]);
+        Console.WriteLine("Ou vai ser esse aqui: {0}", jogos[1]);
+        Console.WriteLine("Ou vai ser esse aqui: {0}", jogos[2]);
+        Console.WriteLine("Ou vai ser esse aqui: {0}", jogos[3]);
+        Console.WriteLine("Entao pode ser esse: {0}", jogos[4]);
+    }
+}
