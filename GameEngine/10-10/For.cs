91211b0caf4bae4bc948a9a2d08efe844d688d8d
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-10-10 16:43:31 -0300

    Create For.cs

---
+++ GameEngine/10-10/For.cs
@@ -0,0 +1,10 @@
+using System;
+
+class For {
+    static void Main (String[] args) {
+
+        for (int voltas = 0; voltas < 10; voltas ++) {
+            Console.WriteLine($"Voce pedalou {voltas} voltas.");
+        }
+    }
+}
