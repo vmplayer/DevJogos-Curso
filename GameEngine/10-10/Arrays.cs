7e05a176032048ada973828e6f962e972711e825
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-10-10 16:43:55 -0300

    Create Arrays.cs

---
+++ GameEngine/10-10/Arrays.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Arrays {
+
+    static void Main (String[] args) {
+
+        //Arrays são listas de valores.
+
+        string[] listaMercado = {"pão", "arroz", "carne"};
+
+        Console.WriteLine(listaMercado[0]);
+/*
+        for (int i = 0; 1 < 3; i++) {
+            Console.WriteLine(listaMercado[0]);
+*/
+        }
+    }
+}
