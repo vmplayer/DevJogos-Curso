6307c2bf962cf089630e92a1c8169d7052149c5b
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-11-07 17:18:55 -0300

    Update ObstacleCollision.cs

--- GameEngine/UnityEngine/Scripts/ObstacleCollision.cs
+++ GameEngine/UnityEngine/Scripts/ObstacleCollision.cs
@@ -2,24 +2,20 @@ using UnityEngine;
 
 public class ObstacleCollision : MonoBehaviour
 {
-    public int health = 100;  // Vida do jogador
+    //public int health = 100;  // Vida do jogador
+    private PlayerStats playerStats; // Referência ao PlayerStats -----------------------
 
-    public AudioClip damageSound;
-    private AudioSource audioSource;
+    private void Start() { // -----------------------
+        playerStats = GetComponent<PlayerStats>(); // -----------------------
+    } // -----------------------
 
-    void Start()
-    {
-        audioSource = gameObject.GetComponent<AudioSource>();
-    }
-    
-    //Quando o Jogador colidir com o obstáculo
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (hit.gameObject.CompareTag("Obstáculo"))
         {
-            health -= 20;  // Reduz a vida
-            Debug.Log("Colidiu com obstáculo! Vida: " + health);
-            audioSource.PlayOneShot(damageSound);
+            playerStats.TakeDamage(10); // -----------------------
+            //health -= 20;  // Reduz a vida
+            //Debug.Log("Colidiu com obstáculo! Vida: " + health);
         }
     }
 }
