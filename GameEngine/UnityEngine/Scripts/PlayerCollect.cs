6154f7603950453c425eab3226777fb8e718b9a5
vmplayer <166970981+vmplayer@users.noreply.github.com>
2024-11-07 16:37:55 -0300

    Update PlayerCollect.cs

--- GameEngine/UnityEngine/Scripts/PlayerCollect.cs
+++ GameEngine/UnityEngine/Scripts/PlayerCollect.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCollect : MonoBehaviour
@@ -10,9 +12,15 @@ public class PlayerCollect : MonoBehaviour
     private ParticleSystem copia;
     public int score = 0; //Armazena a pontuação do jogador
 
+    private PlayerStats playerStats; //Referência ao PlayerStats
+
     private void Start()
     {
+        //Obtém o componente de áudio
         audioSource = gameObject.AddComponent<AudioSource>();
+
+        //Busca o componente PlayerStats no mesmo GameObject
+        playerStats = GetComponent<PlayerStats>();
     }
 
     //Detecta colisão com o coletável
@@ -29,8 +37,9 @@ public class PlayerCollect : MonoBehaviour
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
 
-        if (hit.gameObject.CompareTag("item"))
+        if (hit.gameObject.CompareTag("Coletavel"))
         {
+            playerStats.AddScore(10); //Adiciona a determinada quantia de pontos
             audioSource.PlayOneShot(collectSound);
 
             Instantiate(collectEffect, hit.transform.position, Quaternion.identity);
